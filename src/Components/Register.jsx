581b3d621aa5e0be2890090c25a45eaa77466f9f
adrn <adriangallegos119@gmail.com>
2024-06-11 22:46:19 -0500

    updated register page

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -6,6 +6,7 @@ import { MdEmail } from "react-icons/md";
 import { FaUserShield } from "react-icons/fa";
 import { BsShieldLockFill } from "react-icons/bs";
 import { FaLongArrowAltRight } from "react-icons/fa";
+import logo from '../images/parry-the-platypus.jpg';
 
 export default function Register() {
     const [userData, setUserData] = useState(
@@ -47,62 +48,66 @@ export default function Register() {
       }
 
     return (
-        <div className="formDiv flex">
-        <div className="headerDiv">
-          <h3>Tell Us about Yourself!</h3>
-        </div>
+        <div className="registerPage">
+            <div className="container">
+                <div className="headerDiv">
+                    <h3>Tell Us about Yourself!</h3>
+                    <img className = "logo" src={logo} alt="sexy cool logo"/>
+                </div>
 
-        <form action="" className='form grid' onSubmit={handleSubmit}>
-          {/* <span className='showMessage'>Register Status goes here</span> */}
+                <form action="" className='form grid' onSubmit={handleSubmit}>
+                {/* <span className='showMessage'>Register Status goes here</span> */}
 
-          <div className="inputDiv">
-            <label htmlFor="email">Email</label>
-            <div className="input flex">
-              <MdEmail className ='icon'/>
-              <input type="email"
-                     id='email'
-                     placeholder='Enter Email'
-                     onChange={handleChange}
-                     name="email"
-                     value={userData.email}
-              />
-            </div>
-          </div>
+                <div className="inputDiv">
+                    <label className = "label" htmlFor="email">Email</label>
+                    <div className="input flex">
+                        <MdEmail className ='icon'/>
+                        <input className="input" type="email"
+                        id='email'
+                        placeholder='Enter Email'
+                        onChange={handleChange}
+                        name="email"
+                        value={userData.email}
+                        />
+                    </div>
+                </div>
           
           
-          <div className="inputDiv">
-            <label htmlFor="username">Username</label>
-            <div className="input flex">
-              <FaUserShield className ='icon'/>
-              <input type="text"
-                     id='username'
-                     placeholder='Enter Username'
-                     onChange={handleChange}
-                     name="username"
-                     value={userData.username}
-              />
-            </div>
-          </div>
+                <div className="inputDiv">
+                    <label className = "label" htmlFor="username">Username</label>
+                    <div className="input flex">
+                        <FaUserShield className ='icon'/>
+                        <input className="input" type="text"
+                        id='username'
+                        placeholder='Enter Username'
+                        onChange={handleChange}
+                        name="username"
+                        value={userData.username}
+                        />
+                    </div>
+                </div>
 
-          <div className="inputDiv">
-            <label htmlFor="password">Password</label>
-            <div className="input flex">
-              <BsShieldLockFill className ='icon'/>
-              <input type="password"
-                     id='password'
-                     placeholder='Enter Password'
-                     onChange={handleChange}
-                     name="password"
-                     value={userData.password}
-              />
-            </div>
-          </div>
-          <button className='btn flex'>
-            <span>Register</span>
-            <FaLongArrowAltRight className='icon' />
-          </button>
+                <div className="inputDiv">
+                    <label className = "label" htmlFor="password">Password</label>
+                    <div className="input flex">
+                        <BsShieldLockFill className ='icon'/>
+                        <input className="input" type="password"
+                        id='password'
+                        placeholder='Enter Password'
+                        onChange={handleChange}
+                        name="password"
+                        value={userData.password}
+                        />
+                    </div>
+                </div>
 
-        </form>
-      </div>    
+                <button className='btn'>
+                    <span>Register</span>
+                    <FaLongArrowAltRight className='icon' />
+                </button>
+
+                </form>
+            </div>
+        </div>    
     )
 }
